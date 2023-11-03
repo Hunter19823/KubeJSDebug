79195764d640cd6dcd66383a49dceb1822402ee0
Hunter19823 <hspragg@asu.edu>
2023-11-02 17:53:34 -0700

    Added KubeJS as a dependency

--- common/src/main/java/pie/ilikepiefoo/kubejsdebug/KubeJSDebug.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsdebug/KubeJSDebug.java
@@ -2,9 +2,9 @@ package pie.ilikepiefoo.kubejsdebug;
 
 public class KubeJSDebug
 {
-	public static final String MOD_ID = "kubejsdebug";
+    public static final String MOD_ID = "kubejsdebug";
 
-	public static void init() {
-		
-	}
+    public static void init() {
+
+    }
 }
